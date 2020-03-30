8fe8f420809627870eaa4ec4c4bdf9eff3641e8b
Loui Nissen-Petersen <s194294@student.dtu.dk>
2020-03-30 15:08:33 +0200

    JFX

--- src/JavaFx/openjfx/Controller.java
+++ src/JavaFx/openjfx/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package JavaFx.openjfx;
 
 public class Controller {
 }
--- src/JavaFx/openjfx/Main.java
+++ src/JavaFx/openjfx/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package JavaFx.openjfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
