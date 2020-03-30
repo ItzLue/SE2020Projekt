eb8c0dcaf73eb5ccbf22242f70f58c8747665784
Loui Nissen-Petersen <s194294@student.dtu.dk>
2020-03-30 20:16:08 +0200

    New folders

--- tests/acceptance_tests/steps/DeveloperSteps.java
+++ tests/acceptance_tests/steps/DeveloperSteps.java
@@ -1,5 +1,13 @@
 package acceptance_tests.steps;
 
-public class DeveloperSteps {
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import java.util.Optional;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
-}
