84599d070c895f6f24b8ad7ae21064641688aa25
Regin Kunoy <s183462@student.dtu.dk>
2020-04-09 20:33:10 +0200

    Hubert's løsning til Cucumber

--- test/acceptance_tests/steps/SystemSteps.java
+++ test/acceptance_tests/steps/SystemSteps.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static org.junit.Assert.assertTrue;
+import System.System;
 
 public class SystemSteps {
 
